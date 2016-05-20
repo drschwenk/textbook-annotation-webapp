71c890e053a3c72afc042261790333a4a17e31e8
drschwenk <schwgit2@gmail.com>
2016-05-19 17:55:13 -0700

    Restful api and routing in progress

--- server/api/api.py
+++ server/api/api.py
@@ -4,126 +4,127 @@ from server import jsonify
 import json
 import requests as rq
 import pprint
-from subprocess import call
+from flask_restful import Resource, Api
+
 import utils.url_gen as url_builder
 
+api = Api(app)
 
 book_groups, range_lookup = url_builder.load_book_info()
 
+group_urls = url_builder.make_book_group_urls(book_groups, 'daily_sci', range_lookup)
+group_urls = group_urls[:4]
 
-def valid_request(req):
-    if not req.json:
-        return False
-    if 'description' in req.json and type(req.json['description']) is not str:
-        return False
-    if 'done' in req.json and type(req.json['done']) is not bool:
-        return False
-    if 'order' in req.json and type(req.json['order']) is not int:
-        return False
-    
-    return True
+pages_to_review_idx = range(1, len(group_urls)+1)
 
-
-i_path = 'https://s3-us-west-2.amazonaws.com/ai2-vision-turk-data/textbook-annotation-test/page-images/'
-a_path = 'https://s3-us-west-2.amazonaws.com/ai2-vision-turk-data/textbook-annotation-test/annotations/'
-i1 = 'Daily_Science_Grade_5_Evan_Moor_149.jpeg'
-a1 = 'Daily_Science_Grade_5_Evan_Moor_149.json'
-i2 = 'Spectrum_Science_Grade_7_40.jpeg'
-a2 = 'Spectrum_Science_Grade_7_40.json'
-
-fields = ['id', 'url']
-with open(a1) as f:
-    aj1 = json.load(f)
-    flattened_json = []
-    for a_type, objs in aj1.items():
-        for obj_name, obj in objs.items():
-            obj['type'] = a_type
-            flattened_json.append({obj_name: obj})
-    # print(flattened_json)
+# i_path = 'https://s3-us-west-2.amazonaws.com/ai2-vision-turk-data/textbook-annotation-test/page-images/'
+# a_path = 'https://s3-us-west-2.amazonaws.com/ai2-vision-turk-data/textbook-annotation-test/annotations/'
+# i1 = 'Daily_Science_Grade_5_Evan_Moor_149.jpeg'
+# a1 = 'Daily_Science_Grade_5_Evan_Moor_149.json'
+# i2 = 'Spectrum_Science_Grade_7_40.jpeg'
+# a2 = 'Spectrum_Science_Grade_7_40.json'
 
 
-def flatten_json(annotations):
-    flattened_json = []
-    for a_type, objs in annotations.items():
-        for obj_name, obj in objs.items():
-            obj['type'] = a_type
-            flattened_json.append({obj_name: obj})
-    return flattened_json
+class Image(Resource):
+    fields = ['id', 'url']
 
-@app.route('/api/datasets/1/nextImage', methods=['GET'])
-def get_image():
-    img_val = [1, i_path + i1]
-    img_dict = dict(zip(fields, img_val))
-    img_json =jsonify(img_dict)
-    # print(dir(img_json))
-    # print(img_json.data)
-    return img_json
+    def get(self, image_idx):
+        return_items = [image_idx, group_urls[image_idx]]
+        items_json = jsonify(dict(zip(self.fields, return_items)))
+        return items_json
+
+    def put(self, image_idx):
+        return "test"
+
+
+class Annotation(Resource):
+    def flatten_json(self, annotations):
+        flattened_json = []
+        for a_type, objs in annotations.items():
+            for obj_name, obj in objs.items():
+                obj['type'] = a_type
+                flattened_json.append({obj_name: obj})
+        return flattened_json
+
+    def transform_url(self, url):
+        return url.replace('page-images', 'annotations').replace('jpeg', 'json')
+
+    def get(self, image_idx):
+        image_url = group_urls[image_idx]
+        annotation_url = self.transform_url(image_url)
+        remote_annotation = rq.get(annotation_url)
+        annotation_json = self.flatten_json(json.loads(remote_annotation.content))
+        return annotation_json
+
+    def put(self, image_idx):
+        return 'test'
+
+# class ImageGroup(Resource):
+
+
+class NextImage(Image):
+    def __init__(self):
+        Image.__init__(self)
+
+api.add_resource(Image, '/api/images/<int:image_idx>')
+api.add_resource(Annotation, '/api/images/<int:image_idx>/annotations')
+api.add_resource(NextImage, '/api/datasets/<int:image_idx>/nextImage')
 
 
 @app.route('/api/datasets/1/images', methods=['GET'])
 def get_finished_image():
-    return jsonify([1, 2, 3])
+    return jsonify(pages_to_review_idx)
 
 
-@app.route('/api/nextImage/2', methods=['GET'])
-def get_image2():
-    img_val = [2, i_path + i2]
-    img_dict = dict(zip(fields, img_val))
-    img_json =jsonify(img_dict)
-    # print(dir(img_json))
-    # print(img_json.data)
-    return img_json
 
+# @app.route('/api/datasets/1/nextImage', methods=['GET'])
+# def get_image():
+#     img_val = [1, i_path + i1]
+#     img_dict = dict(zip(fields, img_val))
+#     img_json =jsonify(img_dict)
+#     # print(dir(img_json))
+#     # print(img_json.data)
+#     return img_json
+#
 
-@app.route('/api/images/1', methods=['GET', 'PUT'])
-def get_image_nodata():
-    if request.method == "PUT":
-        # print(request.data)
-        return 'test'
-    else:
-        img_val = [1, i_path + i1]
-        img_dict = dict(zip(fields, img_val))
-        img_json = jsonify(img_dict)
-    return img_json
-
-
-@app.route('/api/images/2', methods=['GET'])
-def get_image_nodata2():
-    img_val = [2, i_path + i2]
-    img_dict = dict(zip(fields, img_val))
-    img_json = jsonify(img_dict)
-    return img_json
-
-
-@app.route('/api/images/1/annotations', methods=['GET', 'POST'])
-def get_annotation():
-    if request.method == 'POST':
-        # labeled_boxes = json.loads(request.data.decode('string-escape'))
-        labeled_boxes = json.loads(json.loads(request.data))
-        # for box in labeled_boxes:
-        #     aj1['text'][box['id']]['category'] = box['category']
-        # with open('example_annotation.json', 'w') as tf:
-        #     json.dump(aj1, tf)
-        return "test"
-    else:
-        response = rq.get(a_path + a1)
-        ann_json = flatten_json(json.loads(response.content))
-    return jsonify(ann_json)
-
-
-@app.route('/api/images/2/annotations', methods=['GET'])
-def get_annotation2():
-    if request.method == 'POST':
-        # labeled_boxes = json.loads(request.data.decode('string-escape'))
-        labeled_boxes = json.loads(json.loads(request.data))
-        for box in labeled_boxes:
-            aj2['text'][box['id']]['category'] = box['category']
-        with open('example_annotation.json', 'w') as tf:
-            json.dump(aj2, tf)
-        return "test"
-    else:
-        response = rq.get(a_path + a2)
-        ann_json = flatten_json(json.loads(response.content))
-    return jsonify(ann_json)
-
+#
+# @app.route('/api/images/1', methods=['GET', 'PUT'])
+# def get_image_nodata():
+#     if request.method == "PUT":
+#         # print(request.data)
+#         return 'test'
+#     else:
+#         img_val = [1, i_path + i1]
+#         img_dict = dict(zip(fields, img_val))
+#         img_json = jsonify(img_dict)
+#     return img_json
+#
+#
+# @app.route('/api/images/2', methods=['GET'])
+# def get_image_nodata2():
+#     img_val = [2, i_path + i2]
+#     img_dict = dict(zip(fields, img_val))
+#     img_json = jsonify(img_dict)
+#     return img_json
+#
+#
+# @app.route('/api/images/1/annotations', methods=['GET', 'POST'])
+# def get_annotation():
+#     if request.method == 'POST':
+#         return "test"
+#     else:
+#         response = rq.get(a_path + a1)
+#         ann_json = flatten_json(json.loads(response.content))
+#     return jsonify(ann_json)
+#
+#
+# @app.route('/api/images/2/annotations', methods=['GET'])
+# def get_annotation2():
+#     if request.method == 'POST':
+#         return "test"
+#     else:
+#         response = rq.get(a_path + a2)
+#         ann_json = flatten_json(json.loads(response.content))
+#     return jsonify(ann_json)
+#
 
